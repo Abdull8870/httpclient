3c753c1f3aa186761aa8c28002dc6ba3ebaa04cd
Abdul Rahuman <abrahuman8870@gmail.com>
2024-07-22 23:32:33 -0400

    Fetching data from backend

--- src/app/places/available-places/available-places.component.ts
+++ src/app/places/available-places/available-places.component.ts
@@ -1,5 +1,5 @@
-import { Component, signal } from '@angular/core';
-
+import { Component, OnInit, signal,inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 import { Place } from '../place.model';
 import { PlacesComponent } from '../places.component';
 import { PlacesContainerComponent } from '../places-container/places-container.component';
@@ -11,6 +11,26 @@ import { PlacesContainerComponent } from '../places-container/places-container.c
   styleUrl: './available-places.component.css',
   imports: [PlacesComponent, PlacesContainerComponent],
 })
-export class AvailablePlacesComponent {
+export class AvailablePlacesComponent implements OnInit {
+
+  private httpClient=inject(HttpClient);
+  isFetching=signal<boolean>(false);
+
+  host:string="http://localhost:3000/places";
+  ngOnInit(): void {
+    this.isFetching.set(true);
+    this.httpClient.get<{places:Place[]}>(this.host).subscribe({
+      next:(res)=>{
+        console.log(res.places);
+        this.places.set(res.places);
+      },
+      complete:()=>{
+        this.isFetching.set(false);
+      }
+    });
+  }
+  
   places = signal<Place[] | undefined>(undefined);
+
+
 }
